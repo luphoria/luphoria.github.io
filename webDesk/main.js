5fc390e3ab1db10c5f4ec28458f05156a6fe7c61
luphoria <luphoria@users.noreply.github.com>
2020-02-04 17:32:18 -0600

    h

--- webDesk/main.js
+++
@@ -1,5 +0,0 @@
-/* webDesk main.js alpha 0.0.1 by luphoria */
-var versionNo = "0.0.1 alpha";
-var devYN = true;
-var minYN = true;
-document.getElementById("webDeskContentSKL").innerHTML = `henlo world`
