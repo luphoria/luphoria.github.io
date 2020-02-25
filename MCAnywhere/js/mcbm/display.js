12189b0a7fd577992d2e9b48385c383b49c04ac8
luphoria <60309933+luphoria@users.noreply.github.com>
2020-02-24 18:08:06 -0600

    Create display.js

---
+++ MCAnywhere/js/mcbm/display.js
@@ -0,0 +1,71 @@
+// ====================================================================
+// Display - setup for canvas display used for bookmarklet 
+//   - handles adding display elements (canvas, container divs, etc.) to DOM
+//   - handles resizing elements 
+//
+// author: Nate Horstmann nate@natehorstmann.com
+// ====================================================================
+
+(function($){ // requires jQuery
+  
+  // ====================================================================
+  // @testDisplay object
+  // ====================================================================
+  MCBM.display = function(showStats, use3D) {
+    
+    var $body = $('body'),
+        displayCanvas = document.createElement('canvas'),
+        displayCanvasCtx = displayCanvas.getContext((use3D === true) ? 'experimental-webgl' : '2d'),
+        $stats = null;
+    
+    var  pScope = {};
+    
+    pScope.canvas = displayCanvas;
+    pScope.dirty = false;
+    
+    displayCanvas.width = MCBM.$container.outerWidth();
+    displayCanvas.height = MCBM.$container.outerHeight();
+    
+    $displayCanvas = $(displayCanvas);
+    $displayCanvas.css({
+      'position':"absolute",
+      'top':'0px',
+      'left':'0px'
+    });
+    MCBM.$container.append($displayCanvas);
+    
+    // framerate display
+    if (showStats) {
+      pScope.stats = new Stats();
+      $stats = $(pScope.stats.domElement);
+      $stats.css({
+        'position': 'absolute',
+        'top': '0px'
+      });
+      MCBM.$container.append( $stats );
+    }
+    
+    pScope.getContext = function() {
+      return displayCanvasCtx;
+    };
+    
+    pScope.getCanvas = function() {
+      return displayCanvas;
+    };
+    
+    pScope.getContainer = function() {
+      return $container;
+    }
+    
+    pScope.clear = function() {
+      
+      displayCanvasCtx.clearRect(0, 0, displayCanvas.width, displayCanvas.height);
+      pScope.dirty = false;
+      
+    }
+    
+    return pScope;
+    
+  };
+  
+})(jQuery);
