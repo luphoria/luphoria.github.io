0142853ee66d0848ea39c760e1a27b8d811fcdc0
luphoria <luphoria@users.noreply.github.com>
2020-03-07 00:45:19 -0600

    add /

--- RockbasherMod/app/CustomLevelBkmlt.js
+++ RockbasherMod/app/CustomLevelBkmlt.js
@@ -1,4 +1,4 @@
 var id = prompt("Enter the name of the custom level you would like to get in to.\nNote if you enter an invalid URL simply nothing will change.");
-var scriptToLoad = "https://luphoria.github.io/RockbasherMod/CustomLevels" + id + ".js";
+var scriptToLoad = "https://luphoria.github.io/RockbasherMod/CustomLevels/" + id + ".js";
 //main script
 (function(){document.body.appendChild(document.createElement('script')).src=scriptToLoad })();
