67612fe5f86529f8801a4368b855ad3857e325c2
luphoria <luphoria@innocent.com>
2021-01-16 23:20:31 -0600

    sync nmjs with commit 5512d65

--- UnfinishedShit/nmjs/museum.js
+++ UnfinishedShit/nmjs/museum.js
@@ -5,48 +5,39 @@ import "./lib/keydrown.min.js"
 var WIDTH = 640
 var HEIGHT = 448
 
-var rd = new THREE.WebGLRenderer({antialias:true}) // creates webgl rendering area
+var rd = new THREE.WebGLRenderer({antialias:false}) // creates webgl rendering area
+var scene = new THREE.Scene()
+var loader = new OBJLoader2()
+var camera = new THREE.PerspectiveCamera(60,WIDTH/HEIGHT) // creates camera
+var dir = new THREE.Vector3()
+
 rd.setSize(WIDTH,HEIGHT) // configs area..
 rd.setClearColor(0xFFFFFF,1)
-window.document.body.appendChild(rd.domElement)
+document.getElementById("gameContainer").appendChild(rd.domElement)
 
-var scene = new THREE.Scene()
-
-var camera = new THREE.PerspectiveCamera(60,WIDTH/HEIGHT) // creates camera
-camera.position.y = 3
-camera.position.z = 140
-var dir = new THREE.Vector3()
 var spd = 1
 var lookUpToggler = false
 scene.add(camera)
 
-console.error("fuck"); console.error("somebody opened the console"); console.error("shit"); console.error("what do i do now"); console.warn("Please do be careful, the console can be used to steal your data! It may also make the game react in odd ways.\n\nNot as much as you may expect can be done here, due to everything being obfuscated on runtime. Hate to get your hopes down.")
-
-// var mvmtModifier = -0.5
-
 /* define functions */
 function render() {
     requestAnimationFrame(render)
+    player.getWorldDirection(dir)
+
     camera.position.x = player.position.x
     camera.position.y = player.position.y
     camera.position.z = player.position.z
-
     camera.rotation.y = player.rotation.y
 
-    if(lookUpToggler) {
-        camera.rotation.x += 0.01
-        camera.rotation.z += 0.01
-    } else {
-        camera.rotation.x -= 0.01
-        camera.rotation.z -= 0.01
+    if(lookUpToggler) { 
+      camera.rotation.x += dir.x * 0.01
+      camera.rotation.z -= dir.z * 0.01
+    } else { 
+      camera.rotation.x -= 0.01 
+      camera.rotation.z -= 0.01
     }
-    if(camera.rotation.x > 0.6 || camera.rotation.z > 0.6) {
-        camera.rotation.x = 0.6
-        camera.rotation.z = 0.6
-    } else if(camera.rotation.x < 0 || camera.rotation.z < 0) {
-        camera.rotation.x = 0
-        camera.rotation.z = 0
-    }
+    if(camera.rotation.x > 0.6) { camera.rotation.x = 0.6 } else if(camera.rotation.x < 0) { camera.rotation.x = 0 }
+    if(camera.rotation.z > 0.6) { camera.rotation.z = 0.6 } else if(camera.rotation.z < 0) { camera.rotation.z = 0 }
     rd.render(scene,camera)
 }
 function getCoords(box,collision) { // Spent way too long trying to make a giant detection for negative/positive, realised i could add a modifier to a "master" return anyways. Think fucking smarter, not harder.
@@ -58,7 +49,7 @@ function collisionCheck() {
     var inc = 0
     while(col.length > inc) {
         if(player.position.x > col[inc][0] && player.position.x < col[inc][3] && player.position.z > col[inc][2] && player.position.z < col[inc][5]) return true
-        inc += 2
+        inc += 1
     }
     return false
 }
@@ -67,25 +58,36 @@ var playergeo = new THREE.BoxGeometry(40,40,40)
 var material = new THREE.MeshBasicMaterial()
 var player = new THREE.Mesh(playergeo,material)
 scene.add(player)
+player.position.x = 160
+player.position.y = 8
+player.position.z = 145
+player.rotation.y = 1.57
 
-var geometry = new THREE.BoxGeometry( 250, 100, 280 )
+var geometry = new THREE.BoxGeometry( 220, 100, 290 )
 var material = new THREE.MeshBasicMaterial( {color: 0xFF00FF} )
-var debugCube = new THREE.Mesh( geometry, material )
-scene.add( debugCube )
-debugCube.position.x += 30
-var debugCubeCollision = getCoords(debugCube,false)
-/*
-var geometry = new THREE.BoxGeometry(25,25,25)
+var colCube1 = new THREE.Mesh( geometry, material )
+scene.add( colCube1 )
+colCube1.position.x -= 30
+var colCube1_c = getCoords(colCube1,false)
+
+var geometry = new THREE.BoxGeometry(100,100,25)
 var material = new THREE.MeshBasicMaterial({color: 0xFFFF00})
-var debugCube2 = new THREE.Mesh(geometry,material)
-scene.add(debugCube2)
-debugCube2.position.x += 80
-var debugCube2Collision = getCoords(debugCube2,true)
-*/
-var col = [debugCubeCollision,"&"/*,debugCube2Collision*/]
+var colCube2 = new THREE.Mesh(geometry,material)
+scene.add(colCube2)
+colCube2.position.x += 120
+colCube2.position.z += 140
+var colCube2_c = getCoords(colCube2,true)
 
+var geometry = new THREE.BoxGeometry(75,100,55)
+var material = new THREE.MeshBasicMaterial({color: 0xFF0000})
+var colCube3 = new THREE.Mesh(geometry,material)
+scene.add(colCube3)
+colCube3.position.x += 100
+colCube3.position.z -= 140
+var colCube3_c = getCoords(colCube3,true)
 
-const loader = new OBJLoader2()
+var col = [colCube1_c,colCube2_c,colCube3_c]
+
 loader.load(
     "./assets/obj/1/OPT.obj",
     function(object) {scene.add(object);object.position.y -= 40;object.scale.set(2.9,2.9,2.9)},
@@ -99,7 +101,7 @@ function move(type,speed) {
             player.position.addScaledVector(dir,-speed)
         }
     } else if (type == "rotate") {
-        player.rotation.y += speed/30
+        player.rotation.y += speed/50
     } else { console.error("ERROR unknown move type " + type) }
 }
 
@@ -110,12 +112,12 @@ kd.D.down(function(){move("rotate",-spd)})
 kd.E.down(function(){lookUpToggler = true})
 kd.E.up(function(){lookUpToggler = false})
 
-var myAudio = new Audio('./assets/sfx/museum.mp3'); 
-myAudio.addEventListener('ended', function() { // Thanks @kingjeffrey on stackoverflow for FF loop support!
+var bgm = new Audio('./assets/sfx/museum.mp3'); 
+bgm.addEventListener('ended', function() { // Thanks @kingjeffrey on stackoverflow for FF loop support!
     this.currentTime = 0;
     this.play();
 }, false);
-myAudio.play();
+bgm.play();
 
 kd.run(function(){kd.tick()})
 render()
