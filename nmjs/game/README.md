ba5109f49df385ed9e69e7c8ea09dbd723ba8064
luphoria <luphoria@innocent.com>
2021-02-11 20:54:44 -0600

    sync NM-JS to commit dec1364

--- nmjs/game/README.md
+++ nmjs/game/README.md
@@ -7,23 +7,23 @@ WIP! Recreation of the first-person interactive museums from the Namco Museum ga
 This is a work-in-progress recreation of all five 3D Namco Museum museums that is currently in very early stages.
 
 ### Who is developing it?
-Really just me, but I'd also lend credit to eros71 for model help, impiaa for reversing the TIP format, and ChewbaccaBox for giving me the texture extraction scripts.
+Really just me, but I'd also lend credit to eros71 for model help, impiaa for reversing the TIP format, and ChewbaccaBox for most/all modeling as well as TIP extraction.
 
 ### Where can I play it?
-A live version can be played at https://luphoria.github.io/unfinished/nmjs/museum
+A live version can be played at https://luphoria.github.io/nmjs/
 
 ## End Goal
-All 5 Namco Museum PS1 3D museums playable with the info-cards. NOT the arcade games included.
+All 5 Namco Museum PS1 3D museums playable. NOT the arcade games included.
 
 ## Currently implemented
- - all models from Volume 1 are here in .obj format 
- - FRO.obj, or the front, is the current playing environment
+ - Vol 1: FRO1, fully textured
+ - Vol 2: OPT2, fully textured
+ - Vol 2: GB2, fully textured
  - Movement: walking, rotating, running
+ - Dynamic music: some levels have different music
  
 ## Known bugs
   - Looking up is completely broken
-  - When locally hosting it, the OBJ loading's % returns as NaN which does not work with the Loading screen
-  - Playing the game while it is loading is completely possible
   
 ## I can't run it locally!
 Firefox security is really shitty and broken for some reason, and it doesn't let you cross-run 
@@ -31,3 +31,12 @@ file:// resources because it isn't HTTP/S. I haven't tested it on Chrome.
 
 ### To fix
 Navigate to `about:config` and toggle `privacy.file_unique_origin`
+
+## Credits
+ - luphoria: code, initial attempts, initial model extracts
+ - ChewbaccaBox: model extracts, TIP reverse engineering, model texturing
+ - impiaa: TIP reverse engineering
+ - eros71: model help
+### Libraries used
+ - three.js
+ - keydrown
--- nmjs/game/levels/selected.js
+++ nmjs/game/levels/selected.js
@@ -2,83 +2,56 @@ import {FRO_entry_1} from "./1/FRO/script.js"
 import {OPT_entry_1} from "./1/OPT/script.js"
 import {OPT_entry_2} from "./2/OPT/script.js"
 import {GB_entry_2} from "./2/GB/script.js"
+import {MAPY_entry_2} from "./2/MAPY/script.js"
+import {CQ_entry_2} from "./2/CQ/script.js"
+import {LOB_entry_2} from "./2/LOB/script.js"
 
-const selected = prompt("Enter level (e.g. \"FRO1\")")
+function setEntry() {
+    var selected = prompt("Enter level (e.g. \"FRO1\")\nenter \"list\" for all levels")
+
+    switch(selected) {
+    case "list":
+        alert("FRO1,OPT1,OPT2,GB2,MAPY2,CQ2,LOB2")
+        return setEntry()
+    case "FRO1": 
+        return FRO_entry_1
+    case "OPT1":
+        return OPT_entry_1
+    case "OPT2":
+        return OPT_entry_2
+    case "GB2":
+        return GB_entry_2
+    case "MAPY2":
+        console.warn("WARN: collision not implemented")
+        return MAPY_entry_2
+    case "CQ2":
+        console.warn("WARN: music not implemented")
+        console.warn("WARN: collision not implemented")
+        return CQ_entry_2
+    case "LOB2": 
+        console.warn("WARN: collision not implemented")
+        return LOB_entry_2
+    default:
+        console.error("unimplemented level \"" + selected + "\", defaulting FRO1")
+        return FRO_entry_1
+    }
+}
+
+var selected_level_entry = setEntry()
 
 export const SelectedLevel = (request) => {
     switch(request) {
         case "pos":
-            switch(selected) {
-                case "FRO1":
-                    return FRO_entry_1[0]
-                case "OPT1":
-                    console.warn("OPT1 is fucked")
-                    return OPT_entry_1[0]
-                case "OPT2":
-                    return OPT_entry_2[0]
-                case "GB2":
-                    return GB_entry_2[0]
-                default:
-                    throw "unimplemented level " + selected
-            }
+            return selected_level_entry[0]
         case "col":
-            switch(selected) {
-                case "FRO1":
-                    return FRO_entry_1[1]
-                case "OPT1":
-                    console.warn("OPT1 is fucked")
-                    return OPT_entry_1[1]
-                case "OPT2":
-                    return OPT_entry_2[1]
-                case "GB2":
-                    return GB_entry_2[1]
-                default:
-                    throw "unimplemented level " + selected
-            }
+            return selected_level_entry[1]
         case "obj":
-            switch(selected) {
-                case "FRO1":
-                    return FRO_entry_1[2]
-                case "OPT1":
-                    console.warn("OPT1 is fucked")
-                    return OPT_entry_1[2]
-                case "OPT2":
-                    return OPT_entry_2[2]
-                case "GB2":
-                    return GB_entry_2[2]
-                default:
-                    throw "unimplemented level " + selected
-            }
+            return selected_level_entry[2]
         case "sfx":
-            switch(selected) {
-                case "FRO1":
-                    return FRO_entry_1[3]
-                case "OPT1":
-                    console.warn("OPT1 is fucked")
-                    return OPT_entry_1[3]
-                case "OPT2":
-                    return OPT_entry_2[3]
-                case "GB2":
-                    return GB_entry_2[3]
-                default:
-                    throw "unimplemented level " + selected
-            }
+            return selected_level_entry[3]
         case "clr":
-            switch(selected) {
-                case "FRO1":
-                    return FRO_entry_1[4]
-                case "OPT1":
-                    console.warn("OPT1 is fucked")
-                    return OPT_entry_1[4]
-                case "OPT2":
-                    return OPT_entry_2[4]
-                case "GB2":
-                    return GB_entry_2[4]
-                default:
-                    throw "unimplemented level " + selected
-            }
+            return selected_level_entry[4]
         default:
             throw "unimplemented request " + request
     }
-
 }
