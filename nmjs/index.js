b8748c33eda16f4369e6d38d21846e434eb2c3d7
Bread <luphoria@innocent.com>
2021-12-12 14:35:31 -0600

    add hypertabs page

--- nmjs/index.js
+++ nmjs/index.js
@@ -1,11 +1,11 @@
-import * as THREE from "./lib/three.module.js"
+import * as THREE from "../lib/three.module.js"
 
-var WIDTH = window.innerWidth
-var HEIGHT = window.innerHeight
+let WIDTH = window.innerWidth
+let HEIGHT = window.innerHeight
 
-var rd = new THREE.WebGLRenderer({antialias:true}) // creates webgl rendering area
-var scene = new THREE.Scene()
-var camera = new THREE.PerspectiveCamera(60,WIDTH/HEIGHT) // creates camera
+let rd = new THREE.WebGLRenderer({antialias:true}) // creates webgl rendering area
+let scene = new THREE.Scene()
+let camera = new THREE.PerspectiveCamera(60,WIDTH/HEIGHT) // creates camera
 
 rd.setSize(WIDTH,HEIGHT) // configs area..
 rd.setClearColor(0x005CE6,1)
@@ -16,11 +16,11 @@ scene.add(camera)
 /* define functions */
 function render() {
     requestAnimationFrame(render)
-    var i = 0
+    let i = 0
     while(i < cubes.length) {
-        var randNum1 = Math.random()
-        var randNum2 = Math.random()
-        var randNum3 = Math.random()
+        let randNum1 = Math.random()
+        let randNum2 = Math.random()
+        let randNum3 = Math.random()
         if(Math.round(randNum1*2) == 0) randNum1 = -randNum1
         if(Math.round(randNum2*2) == 0) randNum2 = -randNum2
         if(Math.round(randNum3*2) == 0) randNum3 = -randNum3
@@ -35,23 +35,23 @@ function render() {
     rd.render(scene,camera)
 }
 
-var counter = 0
-var counterGoal = 120
+let counter = 0
+let counterGoal = 120
 
-var cubes = []
+let cubes = []
 while(counter <= counterGoal) {
-    var cubeX = Math.random() * 69
-    var cubeY = Math.random() * 69
-    var cubeZ = Math.random() * 69
+    let cubeX = Math.random() * 69
+    let cubeY = Math.random() * 69
+    let cubeZ = Math.random() * 69
 
     if(Math.round(cubeX / 69) == 0) cubeX = -cubeX
     if(Math.round(cubeY / 69) == 0) cubeY = -cubeY
     if(Math.round(cubeZ / 69) == 0) cubeZ = -cubeZ
 
-    var geometry = new THREE.BoxGeometry(Math.random() * 20,Math.random() * 20,Math.random() * 20)
-    var material = new THREE.MeshBasicMaterial({color: 0x0066FF})
-    if(counterGoal / 4 > counter) var material = new THREE.MeshBasicMaterial({color: 0x005CE6})
-    var cube = new THREE.Mesh(geometry,material)
+    let geometry = new THREE.BoxGeometry(Math.random() * 20,Math.random() * 20,Math.random() * 20)
+    let material = new THREE.MeshBasicMaterial({color: 0x0066FF})
+    if(counterGoal / 4 > counter) material = new THREE.MeshBasicMaterial({color: 0x005CE6})
+    let cube = new THREE.Mesh(geometry,material)
 
     cube.position.x = cubeX
     cube.position.y = cubeY
@@ -61,7 +61,7 @@ while(counter <= counterGoal) {
 
     counter += 1
 }
-var i = 0
+let i = 0
 while(i < cubes.length) {
     scene.add(cubes[i])
     i += 1
